ad4ead10925ac9b033adb43e741913e9fe2db9d7
sethsatan <sethsatan@hotmail.com>
2017-09-02 20:02:42 +0200

    02-09-2017 20:00

--- CGOL/Error_Model_training.py
+++ CGOL/Error_Model_training.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import random
 import numpy as np
 import tflearn
 from tflearn.layers.core import input_data, dropout, fully_connected
@@ -28,7 +29,7 @@ def neural_network_model(input_size):
     network = fully_connected(network, 128, activation='relu')
     network = dropout(network, 0.8)
 
-    network = fully_connected(network, 3, activation='softmax')
+    network = fully_connected(network, 1, activation='softmax')
     network = regression(network, optimizer='adam', learning_rate=LR,
                          loss='categorical_crossentropy', name='target')
     model = tflearn.DNN(network, tensorboard_dir='log')
@@ -36,8 +37,9 @@ def neural_network_model(input_size):
     return model
 
 def train_model(training_data, model=False):
-    Y = [i[0] for i in training_data]
+    
     X = [i[1] for i in training_data]
+    Y = np.array([i[0] for i in training_data]).reshape(size,1)
     
     if not model:
         model = neural_network_model(input_size = len(X[0]))
@@ -49,9 +51,9 @@ def train_model(training_data, model=False):
 
 with open("BD", "r") as f:
             training_data = []
-            
+            size=0
             for line in f.readlines():
-                
+                size+=1
                 point,row = line.strip().split(":")
                 stat00,foo = row.split(";")
                 stat0 = [[0 for x in range(NX//3)] for y in range(NY)] 
@@ -59,7 +61,14 @@ with open("BD", "r") as f:
                     for x in range(NX//3):
                         value = stat00[((y+x+1)*3)-1]
                         stat0[y][x] = int(value)
-                training_data.append([point,stat0])
+                training_data.append([int(point),stat0])
 
 model = train_model(training_data)
+
+Ran_board = np.array(training_data[0][1]).reshape(1,NY,NX//3)
+print(np.argmax(model.predict(Ran_board)))
+model.save('COGL.model')
+for game in range(1):
+    Ran_board = np.array([[ random.randint(0, 1) for x in range(NX//3)] for y in range(NY)]).reshape(1,NY,NX//3)
+    print(np.argmax(model.predict(Ran_board)))
                                                       
